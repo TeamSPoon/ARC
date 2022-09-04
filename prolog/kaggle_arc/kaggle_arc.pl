305f6c88face33a3aba3d13092a62f1efd4e1442
Douglas R Miles <logicmoo@gmail.com>
2022-09-04 10:53:30 -0700

    arc_start

--- prolog/kaggle_arc/kaggle_arc.pl
+++ prolog/kaggle_arc/kaggle_arc.pl
@@ -96,9 +96,9 @@ quietlyd(G):- quietly(G),!.
 /*
 :- discontiguous '$exported_op'/3. 
 */
-:- multifile '$exported_op'/3. 
-:- dynamic '$exported_op'/3. 
-:- assert(system:('$exported_op'(_,_,_):- fail)).
+:- multifile system:'$exported_op'/3. 
+:- dynamic system:'$exported_op'/3. 
+:- catch((assert(system:('$exported_op'(_,_,_):- fail)),_,true)).
 
 '$pldoc'(_,_,_,_):- fail.
 :- multifile '$pldoc'/4. 
