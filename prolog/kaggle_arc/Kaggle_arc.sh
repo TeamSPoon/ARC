207a13f84ca0d95fb60816036dfdd7422978ebaa
Douglas R Miles <logicmoo@gmail.com>
2022-09-15 06:15:05 -0700

    Kaggle_arc.sh

--- prolog/kaggle_arc/Kaggle_arc.sh
+++ prolog/kaggle_arc/Kaggle_arc.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+SCRIPT=$(readlink -f $0)
+export ARC_DIR=$(dirname $SCRIPT)
+echo ARC_DIR=$ARC_DIR
+cd $ARC_DIR
 rm -rf out
 git checkout out
-swipl -l kaggle_arc || stty sane
+bash -l -c "cd ${ARC_DIR} ; ls ;  swipl ./kaggle_arc.pl -t bfly_starup"  || stty sane
 killall -9 xterm
 stty sane
-
 
