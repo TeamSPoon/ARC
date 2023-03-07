9bb2d6a71d453fb4bde62996daf23616fb26fab1
Douglas R Miles <logicmoo@gmail.com>
2023-03-06 21:15:09 -0800

    arc-dsl
    Former-commit-id: 3138d0045456990a8dce3130aa4519d7ea44f7e3

--- prolog/kaggle_arc/kaggle_arc_imageproc.pl
+++ prolog/kaggle_arc/kaggle_arc_imageproc.pl
@@ -158,28 +158,28 @@ blank(G,G1):- grid_size(G,H,V),make_grid(H,V,G1).
 blank(E,G,G1):- blank(G,G1),mapgrid(=(E),G1).
 
 join_cols([],[]).
-join_cols([Grid1,Grid2],Grid):- is_grid(Grid1), !,append_left(Grid1,Grid2,Grid).
+join_cols([Grid1,Grid2],Grid):- is_grid(Grid1), !,hconcat(Grid1,Grid2,Grid).
 join_cols([Grid|Grids],GridO):- !,join_cols(Grid,Grids,GridO).
 
 join_cols(Grid1,[],Grid1):-!.
 join_cols(Grid1,[Grid2|Grids],Result):-   
-  append_left(Grid1,Grid2,NewGrid),
+  hconcat(Grid1,Grid2,NewGrid),
   join_cols(NewGrid,Grids,Result).
  
 % grow([[sameR,sameR]],[[a,b,c]], [[a,b,c,a,b,c]]).
-append_left(Grid1,[],Grid1):-!.
-append_left(Grid1,Empty,Grid1):- is_empty_grid(Empty),!.
-append_left(Grid1,Grid2,Grid):- length(Grid1,Len),assertion(length(Grid2,Len)),my_maplist(my_append,Grid1,Grid2,Grid).
+hconcat(Grid1,[],Grid1):-!.
+hconcat(Grid1,Empty,Grid1):- is_empty_grid(Empty),!.
+hconcat(Grid1,Grid2,Grid):- length(Grid1,Len),assertion(length(Grid2,Len)),my_maplist(my_append,Grid1,Grid2,Grid).
 
-append_down(Grid1,Grid2,Grid):- my_append(Grid1,Grid2,Grid).
+vconcat(Grid1,Grid2,Grid):- my_append(Grid1,Grid2,Grid).
 
 p2_grow_row([],_,[]).
-p2_grow_row([C1|Row],Grid,GM):- !, call(C1,Grid,G1),p2_grow_row(Row,Grid,GR),append_left(G1,GR,GM).
+p2_grow_row([C1|Row],Grid,GM):- !, call(C1,Grid,G1),p2_grow_row(Row,Grid,GR),hconcat(G1,GR,GM).
 p2_grow([],       _  ,[]).
 p2_grow([Row|Rows],Grid,G1GridO):- p2_grow_row(Row,Grid,G1), p2_grow(Rows,Grid,GridO),my_append(G1,GridO,G1GridO).
 
 p1_grow_row([],_,[]).
-p1_grow_row([C1|Row],Grid,GM):- !, call(C1,G1),p1_grow_row(Row,Grid,GR),append_left(G1,GR,GM).
+p1_grow_row([C1|Row],Grid,GM):- !, call(C1,G1),p1_grow_row(Row,Grid,GR),hconcat(G1,GR,GM).
 p1_grow([],       _  ,[]).
 p1_grow([Row|Rows],Grid,G1GridO):- p1_grow_row(Row,Grid,G1), p1_grow(Rows,Grid,GridO),my_append(G1,GridO,G1GridO).
 
@@ -187,7 +187,7 @@ p1_grow([Row|Rows],Grid,G1GridO):- p1_grow_row(Row,Grid,G1), p1_grow(Rows,Grid,G
 copy_grid_based_on_color(Cell,G,G1):- \+ is_fg_color(Cell),!,grid_size(G,H,V),make_grid(H,V,G1), mapgrid(=(Cell),G1).
 copy_grid_based_on_color(_,G,G1):- safe_grid(G,G1).
 grow_row([],_,[]).
-grow_row([C1|Row],Grid,GM):- !, copy_grid_based_on_color(C1,Grid,G1),grow_row(Row,Grid,GR),append_left(G1,GR,GM).
+grow_row([C1|Row],Grid,GM):- !, copy_grid_based_on_color(C1,Grid,G1),grow_row(Row,Grid,GR),hconcat(G1,GR,GM).
 grow([],       _  ,[]).
 grow([Row|Rows],Grid,G1GridO):- grow_row(Row,Grid,G1), grow(Rows,Grid,GridO),my_append(G1,GridO,G1GridO).
 
